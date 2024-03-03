7cb8f8609f62283dee8e38c1ae53c753b16ad84f
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-03-03 09:51:22 +0100

    Time: 39 ms (86.48%), Space: 17 MB (74.56%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        translator = str.maketrans('', '', string.punctuation)  # make a translation table
+        s = s.translate(translator).lower().replace(" ", "")    # remove all the punctuation and rest
+        L, R = 0, len(s) - 1
+        
+        while L < R:
+            if s[L] != s[R]:
+                return False
+            L += 1
+            R -= 1
+        return True
+        
