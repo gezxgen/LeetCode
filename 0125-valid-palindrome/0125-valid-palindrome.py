10442a5ea4e7a88ac30ed51f2bcc7d47e0013d4b
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-05-18 12:13:41 +0200

    Time: 47 ms (45.29%), Space: 17 MB (63.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,11 +3,6 @@ class Solution:
         translator = str.maketrans('', '', string.punctuation)  # make a translation table
         s = s.translate(translator).lower().replace(" ", "")    # remove all the punctuation and rest
         L, R = 0, len(s) - 1
-        
-        while L < R:
-            if s[L] != s[R]:
-                return False
-            L += 1
-            R -= 1
-        return True
+    
+        return s == s[::-1]
         
