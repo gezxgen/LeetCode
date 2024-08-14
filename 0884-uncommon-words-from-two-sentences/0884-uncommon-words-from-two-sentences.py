e6a5a103baba4bb045842da16471fbee06e3631b
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-08-14 15:38:49 +0200

    Time: 33 ms (75.17%), Space: 16.4 MB (74.35%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -2,23 +2,25 @@ class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
         # make a dict of all words an a counter of occurence
         # return all with a counter of 1
-        words = {}
-        res = []
+        w = {}
+        r = []
+        w1=s1.split(" ")
+        w2=s2.split(" ")
         
-        for word in s1.split(" "):
-            if word in words:
-                words[word] += 1
+        for wd in w1:
+            if wd in w:
+                w[wd] += 1
             else:
-                words[word] = 1
+                w[wd] = 1
         
-        for word in s2.split(" "):
-            if word in words:
-                words[word] += 1
+        for wd in w2:
+            if wd in w:
+                w[wd] += 1
             else:
-                words[word] = 1
+                w[wd] = 1
         
-        for item in words:
-            if words[item] == 1:
-                res.append(item)
-        return res
+        for i in w:
+            if w[i] == 1:
+                r.append(i)
+        return r
         
