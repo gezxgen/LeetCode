8f2664d503bbf15815fff15eca8c97c8ff8da387
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-13 14:04:19 +0200

    Time: 599 ms (79.55%), Space: 18.8 MB (35.76%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,3 +1,5 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        return min(len(set(candyType)), len(candyType) // 2)
+        b = len(candyType) // 2
+        a = len(set(candyType))
+        return min(a, b)
