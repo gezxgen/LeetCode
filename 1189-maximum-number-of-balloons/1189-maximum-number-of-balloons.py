84939330dda1c837cd8dfe1cea0f8d78668ff29e
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-25 08:59:53 +0200

    Time: 43 ms (19.75%), Space: 16.5 MB (72.85%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        # init HashMap (mp)
+        text = text.lower()
+        mp: dict = {}
+        mp["b"] = 0
+        mp["a"] = 0
+        mp["l"] = 0
+        mp["o"] = 0
+        mp["n"] = 0
+        
+        # declare HashMap
+        for txt in text:
+            if txt in mp:
+                mp[txt] += 1
+        
+        # return min -> bottleneck
+        return min(mp["b"], mp["a"], mp["l"]//2, mp["o"]//2, mp["n"])
