200b46979de1a8ae86edb176a49b25fca2781fa3
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-28 11:23:42 +0200

    Time: 55 ms (61.17%), Space: 24.9 MB (6.56%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,33 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        # check empty
-        if (not head) or (not head.next) or (not head.next.next):
-            return head
-        
-        # get middle
-        prev, slow, fast = None, head, head
+    def reverse(self, head):
+        if not head:
+            return None
+        prev = None
+        curr = head
+        nextNode = None
+        while curr:
+            nextNode = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextNode
+        return prev
+
+    def merge(self, list1, list2):
+        while list2:
+            nextNode = list1.next
+            list1.next = list2
+            list1 = list2
+            list2 = nextNode
+
+    def reorderList(self, head):
+        if not head or not head.next:
+            return
+        slow = head
+        fast = head
+        prev = head
         while fast and fast.next:
             prev = slow
-            slow = slow.next
             fast = fast.next.next
+            slow = slow.next
         prev.next = None
-        
-        # reverse second half
-        prev, cur, temp = None, slow, slow.next
-        while temp:
-            temp = cur.next
-            cur.next = prev
-            prev = cur
-            cur = temp
-        
-        # merge both halfs
-        while head.next:
-            temp = head.next    # save next
-            temp2 = prev.next
-            head.next = prev    # realign merge
-            prev.next = temp
-            head = temp         # load save
-            prev = temp2
-        head.next = prev        # assign rest of second half
+        list1 = head
+        list2 = self.reverse(slow)
+        self.merge(list1, list2)
