a68581c7bfffb6c48c62c5194e9c33cf311cd134
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-29 16:16:50 +0200

    Time: 86 ms (94.54%), Space: 17.2 MB (30.34%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -1,22 +1,17 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
+        # create variables and start mapping chars
+        done, flag, cnt = True, set(), 0
         mp = {char: 0 for char in ascii_lowercase}
-        flag: bool = True
-        done: set = set()
-        cnt: int = 0
-
-        for char in chars:
-            if char in mp:
-                mp[char] += 1
+        for char in chars: mp[char] = mp.get(char, 0) + 1
         
+        # check if the word can fit
         for word in words:
-            flag = True
-            done = set()
+            done, flag = set(), True
             for char in word:
                 if (char not in done) and (mp[char] < word.count(char)):
-                    flag = False
-                    break
-                done.add(char)
-            if flag:
-                cnt += len(word)
+                    flag = False        # set false if it can't fit
+                    break               # break out, no need to continue
+                done.add(char)          # append char to the already checked
+            if flag: cnt += len(word)   # append the length of word if it fits
         return cnt
