422711dc32b56af0e953e9d3c86f90ec7aac84f8
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-10-03 21:44:31 +0200

    Time: 44 ms (22.87%), Space: 16.6 MB (46.67%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -4,9 +4,6 @@ class Solution:
             
         for i in range(len(num)-2):
             if num[i] == num[i+1] == num[i+2]:
-                mx = max(mx, int(num[i:i+3]))
+                mx = max(mx, int(num[i]))
         
-        if mx == -1:
-            return ""
-        else:
-            return str(mx).zfill(3)
+        return "" if mx == -1 else str(mx)*3
