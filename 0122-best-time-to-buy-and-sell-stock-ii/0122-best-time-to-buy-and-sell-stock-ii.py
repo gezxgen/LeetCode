90dc83c503a507971b430bd33346faab09a7c58d
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-10-07 10:42:29 +0200

    Time: 61 ms (35.14%), Space: 40.2 MB (5.00%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        mx = 0
-        start = prices[0]
-        len1 = len(prices)
-        for i in range(0 , len1):
-            if start < prices[i]: 
-                mx += prices[i] - start
-            start = prices[i]
-        return mx
+        
+        @cache
+        def trade(day_d):
+            
+            if day_d == 0:
+                
+                # Hold on day_#0 = buy stock at the price of day_#0
+                # Not-hold on day_#0 = doing nothing on day_#0
+                return -prices[day_d], 0
+            
+            prev_hold, prev_not_hold = trade(day_d-1)
+            
+            hold = max(prev_hold, prev_not_hold - prices[day_d] )
+            not_hold = max(prev_not_hold, prev_hold + prices[day_d] )
+            
+            return hold, not_hold
+        
+        # --------------------------------------------------
+        last_day= len(prices)-1
+        
+        # Max profit must come from not_hold state (i.e., no stock position) on last day
+        return trade(last_day)[1]
