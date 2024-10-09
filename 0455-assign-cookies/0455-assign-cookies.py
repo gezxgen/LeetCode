c28f3cc726e4dd816d2098c25e1b26f905959372
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-10-09 16:42:20 +0200

    Time: 310 ms (45.69%), Space: 19.1 MB (71.25%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,20 +1,12 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        if not g or not s:
-            return 0
-        
+        g.sort(), s.sort()
         l1, l2 = len(g), len(s)
-        g.sort()
-        s.sort()
-        j = 0
-        cnt = 0
+        cnt, i, j = 0, 0, 0
         
-        for i in range(l1):
-            while j < l2 - 1 and s[j] < g[i]:
-                j += 1
+        while i < l1 and j < l2:
             if s[j] >= g[i]:
                 cnt += 1
-            if j == l2 - 1:
-                break
-            j += 1
+                i += 1
+            j+=1
         return cnt
