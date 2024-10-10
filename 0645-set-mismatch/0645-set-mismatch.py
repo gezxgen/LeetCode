d6490e1a7285a47a48ce1a92a3bbfd51604e44c7
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-10-10 15:48:08 +0200

    Time: 155 ms (69.41%), Space: 18.6 MB (13.51%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,14 +1,3 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        res, mp = [0, 0], {i: 0 for i in range(len(nums)+1)}
-        
-        for num in nums:
-            mp[num] += 1
-        
-        for i in range(len(mp)):
-            if mp[i] == 0:
-                res[1] = i
-            elif mp[i] == 2:
-                res[0] = i
-        
-        return res
+        return [sum(nums) - sum(set(nums)), sum(range(1, len(nums)+1)) - sum(set(nums))]
