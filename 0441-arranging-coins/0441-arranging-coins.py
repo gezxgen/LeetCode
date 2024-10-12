6be3fb616871f937461bb8e17e687d36db35841a
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-10-12 20:37:31 +0200

    Time: 26 ms (99.04%), Space: 16.4 MB (70.07%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,13 +1,3 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        @lru_cache(None)
-        def meth(n):
-            cnt, i = 0, 1
-        
-            while i <= n:
-                n -= i
-                cnt += 1
-                i += 1
-
-            return cnt
-        return meth(n)
+        return int((pow(1 + 8 * n, 0.5) - 1) / 2)
