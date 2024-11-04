bc760e0873dbb18a7f40c7b1d1dffe51ed906304
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-11-04 09:36:00 +0100

    Time: 143 ms (90.07%), Space: 27.6 MB (15.10%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.py
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -1,12 +1,13 @@
+from itertools import groupby
+
 class Solution:
-    def compressedString(self, word: str) -> str:
-        # create stack of pairs
-        stack, comp = [], ""
-        for char in word:
-            if stack and (stack[-1][0] != char or stack[-1][1] == 9):
-                comp += str(stack[-1][1]) + stack[-1][0]
-            if stack and stack[-1][0] == char and stack[-1][1] != 9:
-                stack[-1][1] += 1
-            else:
-                stack.append([char, 1])
-        return comp + str(stack[-1][1]) + stack[-1][0]
+    def compressedString(self, word: str, ans = "") -> str:
+
+        grps = [(k,len(list(g))) for k, g in groupby(word)]
+        
+        for k, cnt in grps:
+            div, rem = divmod(cnt,9)
+            ans+= ('9'+ k) * div
+            if rem: ans+= str(rem)+k
+
+        return ans
