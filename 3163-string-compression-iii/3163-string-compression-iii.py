4b61ea2bb8a3be56a2adbd88b68c8bc92b0780b0
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-11-04 09:30:27 +0100

    Time: 520 ms (18.44%), Space: 30 MB (8.56%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.py
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -1,15 +1,12 @@
 class Solution:
     def compressedString(self, word: str) -> str:
         # create stack of pairs
-        stack = []
+        stack, comp = [], ""
         for char in word:
+            if stack and (stack[-1][0] != char or stack[-1][1] == 9):
+                comp += str(stack[-1][1]) + stack[-1][0]
             if stack and stack[-1][0] == char and stack[-1][1] != 9:
                 stack[-1][1] += 1
             else:
                 stack.append([char, 1])
-        
-        # create string from stack
-        comp = ""
-        for char, count in stack:
-            comp += str(count) + char
-        return comp
+        return comp + str(stack[-1][1]) + stack[-1][0]
