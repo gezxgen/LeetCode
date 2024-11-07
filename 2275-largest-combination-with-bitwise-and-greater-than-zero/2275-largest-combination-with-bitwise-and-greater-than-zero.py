6557199c233ae1799dc1d0c8d42be37b34e542ba
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-11-07 21:07:07 +0100

    Time: 594 ms (34.12%), Space: 26.8 MB (81.46%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,11 +1,11 @@
 class Solution:
     def largestCombination(self, nums: List[int]) -> int:
         # create a hashmap and count how often each bit is represented, return max
-        mp = {}
+        ls = [0 for _ in range(24)]
         
         for num in nums:
-            for i in range(25):
+            for i in range(24):
                 if num & (1 << i):
-                    mp[i] = mp.get(i, 0) + 1
+                    ls[i] += 1
         
-        return max(mp.values())
+        return max(ls)
