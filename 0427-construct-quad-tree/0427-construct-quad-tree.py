babe589a9efd79beacaec39293ed7c939e62bbff
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-11-23 16:50:28 +0100

    Time: 83 ms (88.16%), Space: 17.4 MB (76.15%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -12,35 +12,14 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
-        def same(grid: List[List[int]]) -> bool:
-            n = len(grid)
-            for i in range(n):
-                for j in range(n):
-                    if grid[i][j] != grid[0][0]:
-                        return False
-            return True
-        
         # check if it is a leaf node
-        if same(grid):
+        if all(cell == grid[0][0] for row in grid for cell in row):
             return Node(bool(grid[0][0]), True)
         
         # if not, return a node with all smaller grids
         n = len(grid) // 2
-        topleft, topright, bottomleft, bottomright = [], [], [], []
-        
-        for i in range(len(grid)):
-            # top half
-            if i < n:
-                topleft.append(grid[i][0:n])
-                topright.append(grid[i][n:])
-            
-            # bottom half
-            else:
-                bottomleft.append(grid[i][0:n])
-                bottomright.append(grid[i][n:])
-        
-        return Node(True, False, self.construct(topleft),
-                                 self.construct(topright),
-                                 self.construct(bottomleft),
-                                 self.construct(bottomright))
+        return Node(True, False, self.construct([row[:n] for row in grid[:n]]),
+                                 self.construct([row[n:] for row in grid[:n]]),
+                                 self.construct([row[:n] for row in grid[n:]]),
+                                 self.construct([row[n:] for row in grid[n:]]))
         
