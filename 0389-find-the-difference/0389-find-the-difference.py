4b596138301edc0e19fe37549c273cad2575ecfd
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-11-28 19:04:42 +0100

    Time: 3 ms (54.64%), Space: 17.5 MB (15.00%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         # hash
-        mp = {}
+        p = 0
         for i in range(len(s)):
-            mp[t[i]] =  mp.get(t[i], 0) + 1
-            mp[s[i]] =  mp.get(s[i], 0) - 1
-        mp[t[-1]] = mp.get(t[-1], 0) + 1
+            p += ord(t[i])
+            p -= ord(s[i])
         
         # find the difference
-        return next(k for k, v in mp.items() if v)
+        return chr(p + ord(t[-1]))
