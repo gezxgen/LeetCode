28fab294aca83904b56314cc6915902b114cef1e
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-12-01 22:50:14 +0100

    Time: 0 ms (100.00%), Space: 17.3 MB (5.26%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -1,13 +1,3 @@
 class Solution:
-    def isGood(self, nums: List[int]) -> bool:
-        nums.sort()
-        
-        if nums[-1] != len(nums) - 1:
-            return False
-        
-        for i, num in enumerate(nums[:-1]):
-            if num != i + 1:
-                return False
-        
-        return True
-        
+    def isGood(self, N):
+        return False if max(N) != len(N) - 1 else all([n == i + 1 for i, n in enumerate(sorted(N)[:-1])])
