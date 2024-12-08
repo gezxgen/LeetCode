03d7dffd13d15ebe58003b400984f1d3a565444a
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-12-08 13:13:22 +0100

    Time: 11 ms (63.54%), Space: 17.4 MB (12.12%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,9 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res, cur = [], []
         
-        def backtracking(nums: List[int], l: int, n: int, i: int, totComb: List[int], curComb: List[int]) -> None:
-            sm = sum(curComb)   # calculate sum of current combination
-            
+        def backtracking(nums: List[int], l: int, n: int, i: int, totComb: List[int], curComb: List[int], sm: int) -> None:
             # append if target is found
             if sm == n:
                 totComb.append(curComb.copy())
@@ -16,11 +14,11 @@ class Solution:
             
             # if it is smaller, include and not include the value
             curComb.append(nums[i])
-            backtracking(nums, l, n, i, totComb, curComb)
+            backtracking(nums, l, n, i, totComb, curComb, sm + nums[i])
             curComb.pop()
             
-            backtracking(nums, l, n, i + 1, totComb, curComb)
+            backtracking(nums, l, n, i + 1, totComb, curComb, sm)
             
 
-        backtracking(candidates, len(candidates), target, 0, res, cur)
+        backtracking(candidates, len(candidates), target, 0, res, cur, 0)
         return res
