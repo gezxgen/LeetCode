de17826b88b2c1f1bbaa41f752d64f304fd0fa53
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-12-13 13:06:48 +0100

    Time: 3 ms (93.45%), Space: 18.8 MB (8.81%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRelativeRanks(self, nums: List[int]) -> List[str]:
+        HashMap, n = {}, len(nums)
+        if n == 1:
+            return ["Gold Medal"]
+        if n == 2:
+            return ["Gold Medal", "Silver Medal"] if nums[0] > nums[1] else ["Silver Medal", "Gold Medal"]
+        
+        # create a hashmap with number: index
+        for i in range(n):
+            HashMap[nums[i]] = i
+        
+        # sort by number
+        HashMap, res = dict(sorted(HashMap.items(), reverse=True)), [""] * n
+        
+        # set the numbers as strings in a result array at index
+        values = list(HashMap.values())
+        res[values[0]] = "Gold Medal"
+        res[values[1]] = "Silver Medal"
+        res[values[2]] = "Bronze Medal"
+        
+        for i in range(3, n):
+            res[values[i]] = str(i + 1)
+        
+        return res
